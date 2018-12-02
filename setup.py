6cf3a14c7f31c42e9e0416f2913c4bc8f00ab0a6
Matthias Blum <mat.blum@gmail.com>
2018-12-02 02:03:31 +0000

    creating setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+from setuptools import setup, Extension
+
+setup(
+    name="pyinterprod",
+    version="1.0",
+    description="",
+    long_description="",
+    ext_modules=[
+        Extension("pyinterprod.sprot",
+                  ["pyinterprod/proteinupdate/sprotmodule.c"],
+                  extra_link_args=["-lsqlite3"])
+    ],
+    zip_safe=False
+)
