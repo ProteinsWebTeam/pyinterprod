177349c09e7265366eb8bbd887a48bf419cbd079
Matthias Blum <mat.blum@gmail.com>
2019-01-28 15:53:19 +0000

    Init workflow

--- pyinterprod/proteinupdate/__init__.py
+++ pyinterprod/proteinupdate/__init__.py
@@ -1,2 +1,43 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+
+def main():
+    import argparse
+    import json
+    import os
+    from tempfile import gettempdir
+
+    from ..orautils import create_db_links
+    from . import proteins, matches, uniparc
+
+    parser = argparse.ArgumentParser(description="InterPro protein update")
+    parser.add_argument("config", metavar="CONFIG.JSON",
+                        help="config JSON file")
+    parser.add_argument("-t", "--tmp", metavar="DIRECTORY",
+                        help="temporary directory", default=gettempdir())
+    args = parser.parse_args()
+
+    try:
+        with open(args.config, "rt") as fh:
+            config = json.load(fh)
+    except FileNotFoundError:
+        parser.error("{}: no such file or directory".format(args.config))
+
+    try:
+        os.makedirs(args.tmp, exist_ok=True)
+    except Exception as e:
+        parser.error(e)
+
+    interpro_url = config["databases"]["interpro"]
+    iprscan_url = config["databases"]["iprscan"]
+    uniparc_url = config["databases"]["uniparc"]
+    swissprot_ff = config["flat_files"]["swissprot"]
+    trembl_ff = config["flat_files"]["trembl"]
+
+    create_db_links(interpro_url, [iprscan_url, uniparc_url])
+    uniparc.update(interpro_url)
+    proteins.track_changes(interpro_url, swissprot_ff, trembl_ff, dir=args.tmp)
+    proteins.delete(interpro_url, truncate=True)
+    proteins.refresh_sequences_to_scan(interpro_url)
+    matches.import_ispro(interpro_url)
