1d0aac658240a8591534eba927c5c71ef5ccde7b
Matthias Blum <mat.blum@gmail.com>
2019-02-19 18:51:35 +0000

    logging stuff

--- pyinterprod/__init__.py
+++ pyinterprod/__init__.py
@@ -1,13 +1,14 @@
 import logging
 
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-_ch = logging.StreamHandler()
-_ch.setFormatter(
-    logging.Formatter(
-        fmt='%(asctime)s: %(levelname)s: %(message)s',
-        datefmt='%y-%m-%d %H:%M:%S'
+
+if not logger.hasHandlers():
+    logger.setLevel(logging.INFO)
+    _ch = logging.StreamHandler()
+    _ch.setFormatter(
+        logging.Formatter(
+            fmt='%(asctime)s: %(levelname)s: %(message)s',
+            datefmt='%y-%m-%d %H:%M:%S'
+        )
     )
-)
-logger.addHandler(_ch)
-#logger.propagate = False
+    logger.addHandler(_ch)
