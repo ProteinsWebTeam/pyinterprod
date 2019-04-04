9438db096f0389bda4572a92601a36c4d494658e
Matthias Blum <mat.blum@gmail.com>
2019-04-04 22:01:56 +0100

    Function to build XREF_SUMMARY and export dat/tab files

--- pyinterprod/proteinupdate/xref.py
+++ pyinterprod/proteinupdate/xref.py
@@ -1,5 +1,7 @@
-import cx_Oracle
+import os
+from typing import Tuple
 
+import cx_Oracle
 
 from .. import logger, orautils
 
@@ -55,16 +57,16 @@ def build_condensed_matches(url: str):
             POS_TO NUMBER(5) NOT NULL
         )
         PARTITION BY LIST (ENTRY_TYPE) (
-          PARTITION XC_TYPE_A VALUES ('A'),
-          PARTITION XC_TYPE_B VALUES ('B'),
-          PARTITION XC_TYPE_C VALUES ('C'),
-          PARTITION XC_TYPE_D VALUES ('D'),
-          PARTITION XC_TYPE_F VALUES ('F'),
-          PARTITION XC_TYPE_H VALUES ('H'),
-          PARTITION XC_TYPE_P VALUES ('P'),
-          PARTITION XC_TYPE_R VALUES ('R'),
-          PARTITION XC_TYPE_U VALUES ('U')
-        )
+          PARTITION PART_A VALUES ('A'),
+          PARTITION PART_B VALUES ('B'),
+          PARTITION PART_C VALUES ('C'),
+          PARTITION PART_D VALUES ('D'),
+          PARTITION PART_F VALUES ('F'),
+          PARTITION PART_H VALUES ('H'),
+          PARTITION PART_P VALUES ('P'),
+          PARTITION PART_R VALUES ('R'),
+          PARTITION PART_U VALUES ('U')
+        ) NOLOGGING 
         """
     )
 
@@ -96,7 +98,7 @@ def build_condensed_matches(url: str):
     _protein_acc = None
     num_proteins = 0
     query = """
-      INSERT INTO INTERPRO.XREF_CONDENSED
+      INSERT /*+ APPEND */ INTO INTERPRO.XREF_CONDENSED
       VALUES (:1, :2, :3, :4, :5, :6)
     """
     table = orautils.TablePopulator(con, query, autocommit=True)
@@ -169,8 +171,7 @@ def build_condensed_matches(url: str):
         """
     )
 
-    cur.execute("GRANT SELECT ON INTERPRO.XREF_CONDENSED TO KRAKEN")
-
+    orautils.grant(cur, "INTERPRO", "XREF_CONDENSED", "SELECT", "KRAKEN")
     cur.close()
     con.close()
     logger.info("proteins processed: {:>15}".format(num_proteins))
@@ -180,7 +181,218 @@ def build_summary(url: str):
     logger.info("building XREF_SUMMARY")
     con = cx_Oracle.connect(url)
     cur = con.cursor()
+    # orautils.truncate_table(cur, "INTERPRO", "XREF_SUMMARY")
     orautils.drop_table(cur, "INTERPRO", "XREF_SUMMARY")
+    cur.execute(
+        """
+        CREATE TABLE INTERPRO.XREF_SUMMARY
+        (
+            DBCODE CHAR(1) NOT NULL,
+            PROTEIN_AC VARCHAR2(15) NOT NULL,
+            ENTRY_AC VARCHAR2(9) NOT NULL,
+            SHORT_NAME VARCHAR2(30),
+            METHOD_AC VARCHAR2(25) NOT NULL,
+            METHOD_NAME VARCHAR2(100),
+            POS_FROM NUMBER(5) NOT NULL,
+            POS_TO NUMBER(5) NOT NULL,
+            MATCH_STATUS CHAR(1) NOT NULL,
+            SCORE FLOAT(*),
+            FRAGMENTS VARCHAR2(400)
+        )
+        PARTITION BY LIST (DBCODE) (
+          PARTITION PART_B VALUES ('B'),
+          PARTITION PART_D VALUES ('D'),
+          PARTITION PART_F VALUES ('F'),
+          PARTITION PART_H VALUES ('H'),
+          PARTITION PART_J VALUES ('J'),
+          PARTITION PART_M VALUES ('M'),
+          PARTITION PART_N VALUES ('N'),
+          PARTITION PART_P VALUES ('P'),
+          PARTITION PART_Q VALUES ('Q'),
+          PARTITION PART_R VALUES ('R'),
+          PARTITION PART_U VALUES ('U'),
+          PARTITION PART_V VALUES ('V'),
+          PARTITION PART_X VALUES ('X'),
+          PARTITION PART_Y VALUES ('Y')
+        ) COMPRESS NOLOGGING 
+        """
+    )
 
+    cur.execute(
+        """
+        INSERT /*+ APPEND */ INTO INTERPRO.XREF_SUMMARY
+        SELECT 
+          MA.DBCODE,
+          MA.PROTEIN_AC,
+          E.ENTRY_AC,
+          E.SHORT_NAME,
+          MA.METHOD_AC,
+          ME.NAME,
+          MA.POS_FROM,
+          MA.POS_TO,
+          MA.STATUS,
+          MA.SCORE,
+          MA.FRAGMENTS
+        FROM INTERPRO.MATCH MA
+        INNER JOIN INTERPRO.METHOD ME ON MA.METHOD_AC = ME.METHOD_AC
+        INNER JOIN INTERPRO.ENTRY2METHOD EM ON ME.METHOD_AC = EM.METHOD_AC
+        INNER JOIN INTERPRO.ENTRY E ON EM.ENTRY_AC = E.ENTRY_AC
+        WHERE EM.EVIDENCE != 'EXC'
+        AND E.CHECKED = 'Y'
+        """
+    )
+    con.commit()
+
+    logger.info("gathering statistics")
+    orautils.gather_stats(cur, "INTERPRO", "XREF_SUMMARY")
+
+    logger.info("creating indexes")
+    for col in ("PROTEIN_AC", "ENTRY_AC", "METHOD_AC"):
+        cur.execute(
+            """
+            CREATE INDEX I_XREF_SUMMARY${0}
+            ON INTERPRO.XREF_SUMMARY ({0}) NOLOGGING
+            """.format(col)
+        )
+
+    orautils.grant(cur, "INTERPRO", "XREF_SUMMARY", "SELECT", "KRAKEN")
     cur.close()
     con.close()
+
+
+def check_summary(url: str):
+    logger.info("checking CRC64 mismatches")
+    con = cx_Oracle.connect(url)
+    cur = con.cursor()
+    cur.execute(
+        """
+        SELECT COUNT(*)
+        FROM INTERPRO.XREF_SUMMARY XS
+        INNER JOIN INTERPRO.PROTEIN IP 
+          ON XS.PROTEIN_AC = IP.PROTEIN_AC
+        INNER JOIN UNIPARC.XREF UX 
+          ON XS.PROTEIN_AC = UX.AC AND UX.DELETED = 'N'
+        INNER JOIN UNIPARC.PROTEIN UP
+          ON UX.UPI = UP.UPI
+        INNER JOIN INTERPRO.PROTEIN IP ON UX.AC = IP.PROTEIN_AC
+        WHERE IP.CRC64 != UP.CRC64
+        """
+    )
+    num_miscmatches = cur.fetchone()[0]
+    cur.close()
+    con.close()
+
+    if num_miscmatches:
+        raise RuntimeError("{} CRC64 mismatches".format(num_miscmatches))
+    else:
+        logger.info("no CRC64 mismatches")
+
+
+def export_databases(url: str, dst: str):
+    logger.info("exporting dat/tab files")
+    con = cx_Oracle.connect(url)
+    cur = con.cursor()
+    cur.execute(
+        """
+        SELECT 
+          PROTEIN_AC, 
+          METHOD_AC, 
+          MIN(METHOD_NAME),
+          MIN(DBCODE),
+          MIN(ENTRY_AC), 
+          MIN(SHORT_NAME), 
+          COUNT(*)
+        FROM INTERPRO.XREF_SUMMARY
+        GROUP BY PROTEIN_AC, METHOD_AC
+        ORDER BY PROTEIN_AC
+        """
+    )
+
+    dbcodes = {
+        'B': ("SFLD", "SFLD"),
+        'D': ("ProDom", "PD"),
+        'F': ("PRINTS", "PP"),
+        'H': ("Pfam", "PF"),
+        'J': ("CDD", "CDD"),
+        'M': ("PROSITE", "PR"),
+        'N': ("TIGRFAMs", "TF"),
+        'P': ("PROSITE", "PR"),
+        'Q': ("HAMAP", "HP"),
+        'R': ("SMART", "SM"),
+        'U': ("PIRSF", "PI"),
+        'V': ("PANTHER", "PTHR"),
+        'X': ("Gene3D", "G3D"),
+        'Y': ("SUPFAM", "SF"),
+    }
+
+    files = {}
+    for dbcode in dbcodes:
+        if dbcode != 'P':
+            # P (PROSITE patterns) -> same file than M (PROSITE profiles)
+            dbname, dbkey = dbcodes[dbcode]
+            fh1 = open(os.path.join(dst, dbname.lower() + ".tab"), "wt")
+            fh2 = open(os.path.join(dst, dbname + ".dat"), "wt")
+            files[dbcode] = (fh1, fh2)
+
+    files['P'] = files['M']
+
+    ifh1 = open(os.path.join(dst, "interpro.tab"), "wt")
+    ifh2 = open(os.path.join(dst, "InterPro.dat"), "wt")
+
+    entries = {}
+    _protein = None
+    for row in cur:
+        protein_acc = row[0]
+        signature_acc = row[1]
+        signature_name = row[2]
+        dbcode = row[3]
+        entry_acc = row[4]
+        entry_name = row[5]
+        num_matches = int(row[6])
+
+        dbname, dbkey = dbcodes[dbcode]
+        fh1, fh2 = files[dbcode]
+
+        row = (protein_acc, dbkey, signature_acc, signature_name, num_matches)
+        fh1.write("{}\t{}\t{}\t{}\t{}\n".format(*row))
+
+        if dbcode == 'X':
+            # Gene3D: transform accession and do not print signature name
+            fh2.write("{}   DR   {}; {}; -; {}.\n".format(*convert_gene3d(row)))
+        elif dbcode == 'F':
+            # PRINTS: do not print match count
+            fh2.write("{}   DR   {}; {}; {}.\n".format(*row))
+        else:
+            fh2.write("{}   DR   {}; {}; {}; {}\n.".format(*row))
+
+        if protein_acc != _protein:
+            for _entry in sorted(entries):
+                row = (_protein, _entry, entries[_entry])
+                ifh1.write("{}\t{}\t{}\n".format(*row))
+                ifh2.write("{}    DR   InterPro; {}; {}.\n".format(*row))
+
+            entries = {}
+            _protein = protein_acc
+
+        if entry_acc not in entries:
+            entries[entry_acc] = entry_name
+
+    for _entry in sorted(entries):
+        row = (_protein, _entry, entries[_entry])
+        ifh1.write("{}\t{}\t{}\n".format(*row))
+        ifh2.write("{}    DR   InterPro; {}; {}.\n".format(*row))
+
+    ifh1.close()
+    ifh2.close()
+
+    for fh1, fh2 in files.values():
+        fh1.close()
+        fh2.close()
+
+    logger.info("complete")
+
+
+def convert_gene3d(row: Tuple[str, str, str, str, int]) -> Tuple[str, str,
+                                                                 str, int]:
+    # G3DSA:3.90.1580.10 -> 3.90.1580.10
+    return row[0], row[1], row[2][6:], row[4]
