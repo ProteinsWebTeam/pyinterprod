31597687acd29f264a799b136a6f1dc7cc155d23
Matthias Blum <mat.blum@gmail.com>
2019-04-18 16:40:22 +0100

    Adding prototype to refresh UNIPARC.PROTEIN

--- pyinterprod/proteinupdate/uniparc.py
+++ pyinterprod/proteinupdate/uniparc.py
@@ -4,15 +4,103 @@ from .. import logger, orautils
 
 
 def update(user: str, dsn: str):
-    logger.info("creating UNIPARC.XREF")
-    
     con = cx_Oracle.connect(orautils.make_connect_string(user, dsn))
     cur = con.cursor()
-    
+    _update_xref(cur)
+    _update_database(cur)
+    cur.close()
+    con.close()
+
+
+def _update_database(cur: cx_Oracle.Cursor):
+    logger.info("creating UNIPARC.CV_DATABASE")
+
+    orautils.drop_table(cur, "UNIPARC", "CV_DATABASE")
+    cur.execute(
+        """
+        CREATE TABLE UNIPARC.CV_DATABASE 
+        TABLESPACE UNIPARC_TAB
+        NOLOGGING 
+        AS
+        SELECT * 
+        FROM UNIPARC.CV_DATABASE@UAREAD
+        """
+    )
+    orautils.grant(cur, "UNIPARC", "CV_DATABASE", "SELECT", "PUBLIC")
+
+    logger.info("indexing UNIPARC.CV_DATABASE")
+    cur.execute(
+        """
+        CREATE UNIQUE INDEX PK_CV_DATABASE
+        ON UNIPARC.CV_DATABASE (ID)
+        TABLESPACE UNIPARC_IND
+        NOLOGGING
+        """
+    )
+    cur.execute(
+        """
+        CREATE UNIQUE INDEX UQ_CV_DATABASE$DESCR
+        ON UNIPARC.CV_DATABASE (DESCR)
+        TABLESPACE UNIPARC_IND
+        NOLOGGING
+        """
+    )
+    cur.execute(
+        """
+        ALTER TABLE UNIPARC.CV_DATABASE
+        ADD (
+          CONSTRAINT PK_CV_DATABASE PRIMARY KEY(ID)
+            USING INDEX PK_CV_DATABASE,
+          CONSTRAINT UQ_CV_DATABASE$DESCR UNIQUE (DESCR)
+            USING INDEX UQ_CV_DATABASE$DESCR
+        )
+        """
+    )
+
+    logger.info("UNIPARC.CV_DATABASE ready")
+
+
+def _update_protein(user: str, dsn: str):
+    logger.info("creating UNIPARC.PROTEIN")
+
+    con = cx_Oracle.connect(orautils.make_connect_string(user, dsn))
+    cur = con.cursor()
+    orautils.drop_table(cur, "UNIPARC", "PROTEIN")
+    orautils.drop_mview(cur, "UNIPARC", "PROTEIN")
+    cur.execute(
+        """
+        CREATE TABLE UNIPARC.PROTEIN 
+        TABLESPACE UNIPARC_TAB 
+        NOLOGGING
+        AS
+        SELECT 
+            UPI, TIMESTAMP, USERSTAMP, CRC64, LEN, SEQ_SHORT, SEQ_LONG, MD5
+        FROM UNIPARC.PROTEIN@UAPRO
+        """
+    )
+    orautils.grant(cur, "UNIPARC", "PROTEIN", "SELECT", "PUBLIC")
+
+    logger.info("indexing UNIPARC.PROTEIN")
+    cur.execute(
+        """
+        CREATE UNIQUE INDEX PK_PROTEIN
+        ON UNIPARC.PROTEIN (UPI)
+        TABLESPACE UNIPARC_IND 
+        """
+    )
+
+    cur.close()
+    con.close()
+
+    logger.info("UNIPARC.PROTEIN ready")
+
+
+def _update_xref(cur: cx_Oracle.Cursor):
+    logger.info("creating UNIPARC.XREF")
+
     # XREF_OLD: legacy table, just to be sure it does not exist any more
     orautils.drop_table(cur, "UNIPARC", "XREF")
     orautils.drop_table(cur, "UNIPARC", "XREF_OLD")
-
     cur.execute(
         """
         CREATE TABLE UNIPARC.XREF 
@@ -24,7 +112,8 @@ def update(user: str, dsn: str):
         """
     )
     orautils.grant(cur, "UNIPARC", "XREF", "SELECT", "PUBLIC")
-                        
+
+    logger.info("indexing UNIPARC.XREF")
     cur.execute(
         """
         CREATE INDEX I_XREF$UPI
@@ -50,45 +139,4 @@ def update(user: str, dsn: str):
         """
     )
 
-    logger.info("creating UNIPARC.CV_DATABASE")
-    try:
-        cur.execute("DROP TABLE UNIPARC.CV_DATABASE")
-    except cx_Oracle.DatabaseError as e:
-        if e.args[0].code != 942:
-            # ORA-00942: table or view does not exist
-            raise e
-    cur.execute(
-        """
-        CREATE TABLE UNIPARC.CV_DATABASE TABLESPACE UNIPARC_TAB AS
-        SELECT *
-        FROM UNIPARC.CV_DATABASE@UAREAD
-        """
-    )
-    orautils.grant(cur, "UNIPARC", "CV_DATABASE", "SELECT", "PUBLIC")
-    cur.execute(
-        """
-        CREATE UNIQUE INDEX PK_CV_DATABASE
-        ON UNIPARC.CV_DATABASE (ID)
-        TABLESPACE UNIPARC_IND
-        """
-    )
-    cur.execute(
-        """
-        CREATE UNIQUE INDEX UQ_CV_DATABASE$DESCR
-        ON UNIPARC.CV_DATABASE (DESCR)
-        TABLESPACE UNIPARC_IND
-        """
-    )
-    cur.execute(
-        """
-        ALTER TABLE UNIPARC.CV_DATABASE
-        ADD (
-          CONSTRAINT PK_CV_DATABASE PRIMARY KEY(ID)
-            USING INDEX PK_CV_DATABASE,
-          CONSTRAINT UQ_CV_DATABASE$DESCR UNIQUE (DESCR)
-            USING INDEX UQ_CV_DATABASE$DESCR
-        )
-        """
-    )
-    cur.close()
-    con.close()
+    logger.info("UNIPARC.XREF ready")
