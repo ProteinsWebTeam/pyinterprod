1e7ec710626e8a484032ef19a03fa4567b50bdc8
Matthias Blum <mat.blum@gmail.com>
2019-05-26 21:59:32 +0100

    update requirements

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
                   ["pyinterprod/proteinupdate/sprotmodule.c"],
                   extra_link_args=["-lsqlite3"])
     ],
-    install_requires=["cx-Oracle>=7.0"],
+    install_requires=["cx-Oracle>=7.0", "mundone>=0.2.0"],
     zip_safe=False,
     entry_points={
         "console_scripts": [
