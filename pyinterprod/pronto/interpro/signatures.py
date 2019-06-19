0ced0cb8d33e93c1cad3df9ff821235436482094
Matthias Blum <mat.blum@gmail.com>
2019-06-19 23:25:50 +0100

    probably last attempt to speed things u

--- pyinterprod/pronto/interpro/signatures.py
+++ pyinterprod/pronto/interpro/signatures.py
@@ -5,147 +5,188 @@ from multiprocessing import Process, Queue
 from typing import Callable, List, Optional, Tuple
 
 from ... import logger
-from .utils import merge_comparators, merge_kvdbs, Kvdb
+from .utils import Kvdb, PersistentBuffer
 
 
-def _process2(database: str, task_queue: Queue, done_queue: Queue,
-              dir: Optional[str]=None, buffer_size: int=0):
-    with Kvdb(database) as src, Kvdb(dir=dir, buffer_size=buffer_size) as dst:
-        for acc_1, values_1 in iter(task_queue.get, None):
-            comparisons = {}
-            counts = {k: len(values) for k, values in values_1.items()}
-            it = src.range(acc_1)
-            next(it)
-            for acc_2, values_2 in it:
-                comparisons[acc_2] = {}
-                for k, values in values_1.items():
-                    if k in values_2:
-                        comparisons[acc_2][k] = len(values & values_2[k])
-                    else:
-                        comparisons[acc_2][k] = 0
+def process(dir: Optional[str], task_queue: Queue, max_items: int,
+            done_queue: Queue):
+    with PersistentBuffer(dir) as buffer:
+        counts = {}
+        comparisons = {}
+        num_items = 0
+        for accessions in iter(task_queue.get, None):
+            for i, acc_1 in enumerate(accessions):
+                try:
+                    counts[acc_1] += 1
+                except KeyError:
+                    counts[acc_1] = 1
 
-            dst[acc_1] = (counts, comparisons)
+                try:
+                    cmp = comparisons[acc_1]
+                except KeyError:
+                    cmp = comparisons[acc_1] = {}
 
-    done_queue.put(dst.filepath)
+                for acc_2 in accessions[i+1:]:
+                    try:
+                        cmp[acc_2] += 1
+                    except KeyError:
+                        cmp[acc_2] = 1
+                        num_items += 1
+
+                if max_items and num_items >= max_items:
+                    buffer.add(comparisons)
+                    comparisons = {}
+                    num_items = 0
+
+        buffer.add(comparisons)
+
+    done_queue.put((counts, buffer))
 
 
-def _process(database: str, task_queue: Queue, done_queue: Queue,
-             dir: Optional[str]=None, buffer_size: int=0):
-    with Kvdb(database) as src, Kvdb(dir=dir, buffer_size=buffer_size) as dst:
-        for acc_1, values_1 in iter(task_queue.get, None):
-            comparisons = {}
-            it = src.range(acc_1)
-            next(it)
-            for acc_2, values_2 in it:
-                comparisons[acc_2] = len(values_1 & values_2)
-
-            dst[acc_1] = (len(values_1), comparisons)
-
-    done_queue.put(dst)
-
-
-def _compare(fn: Callable, database: str, processes: int, **kwargs) -> Tuple[Kvdb, int]:
-    task_queue = Queue(maxsize=1)
-    done_queue = Queue()
+def compare(user: str, dsn: str, query: str, processes: int,
+            dir: Optional[str], max_items: int, chunk_size: int):
     pool = []
+    task_queue = Queue(maxsize=max(1, processes-1))
+    done_queue = Queue()
     for _ in range(max(1, processes-1)):
-        p = Process(target=fn,
-                    args=(database, task_queue, done_queue),
-                    kwargs=kwargs)
+        p = Process(target=process, args=(dir, task_queue, max_items, done_queue))
         p.start()
         pool.append(p)
 
-    cnt = 0
-    with Kvdb(database) as kvdb:
-        for acc_1, values_1 in kvdb:
-            task_queue.put((acc_1, values_1))
-            cnt += 1
-            if not cnt % 10000:
-                logger.debug("{:>15}".format(cnt))
+    logger.debug("starting")
+    con = cx_Oracle.connect(user + '@' + dsn)
+    cur = con.cursor()
+    cur.execute(query)
+    _key = None
+    accessions = []
+    i = 0
+    for key, acc in cur:
+        if key != _key:
+            task_queue.put(accessions)
+            accessions = []
+            _key = key
 
-        size_old = kvdb.size
-        # kvdb.remove()
+        accessions.append(acc)
+        i += 1
+        if not i % 10000000:
+            logger.debug("{:>12,}".format(i))
+
+    task_queue.put(accessions)
+    accessions = []
+    logger.debug("{:>12,}".format(i))
+    cur.close()
+    con.close()
 
     for _ in pool:
         task_queue.put(None)
 
-    kvdbs = []
-    size = 0
-    for _ in pool:
-        kvdb = done_queue.get()
-        kvdbs.append(kvdb)
-        size += kvdb.size
+    logger.debug("collecting")
+    counts = {}
+    buffers = []
+    for i, p in enumerate(pool):
+        _counts, buffer = done_queue.get()
+        for acc, cnt in _counts.items():
+            try:
+                counts[acc] += cnt
+            except KeyError:
+                counts[acc] = cnt
+
+        buffers.append(buffer)
+        logger.debug("collected: {}/{} ({} chunks)".format(i+1, len(pool), len(buffer)))
 
     for p in pool:
         p.join()
 
-    with Kvdb(dir=kwargs.get("dir")) as kvdb:
-        for key, values in merge_kvdbs(kvdbs, remove=False):
-            kvdb[key], = values  # one single item
+    logger.debug("aggregating")
+    keys = sorted(counts.keys())
+    keys = [keys[i] for i in range(0, len(keys), chunk_size)]
+    organizer = utils.Organizer(keys, dir=dir)
+    size = 0
+    num_items = 0
+    for i, buffer in enumerate(buffers):
+        for comparisons in buffer:
+            for acc, cmps in comparisons.items():
+                organizer.add(acc, cmps)
+                num_items += len(cmps)
 
-        size_new = kvdb.size
+                if num_items >= max_items * processes:
+                    organizer.flush()
+                    num_items = 0
 
-    return kvdb, size + max(size_old, size_new)
+        size += buffer.size
+        buffer.remove()
+        logger.debug("merged: {}/{}".format(i+1, len(buffers)))
+
+    logger.debug("merging")
+    size += organizer.merge(processes, fn=sum_counts)
+
+    logger.debug("total: {:.0f} MB".format(size/1024**2))
 
 
-def compare_descriptions(*args, **kwargs) -> Tuple[Kvdb, int]:
+def cmp_descriptions(user: str, dsn: str, processes: int=1,
+                     dir: Optional[str]=None, max_items: int=10000000,
+                     chunk_size: int=100):
     return _compare(_process, *args, **kwargs)
 
 
-def compare_taxa(*args, **kwargs) -> Tuple[Kvdb, int]:
+def cmp_taxa(user: str, dsn: str, processes: int=1,
+             dir: Optional[str]=None, max_items: int=10000000,
+             chunk_size: int=100):
     return _compare(_process2, *args, **kwargs)
 
 
-def compare_terms(*args, **kwargs) -> Tuple[Kvdb, int]:
+def cmp_terms(user: str, dsn: str, processes: int=1,
+              dir: Optional[str]=None, max_items: int=10000000,
+              chunk_size: int=100):
     return _compare(_process, *args, **kwargs)
 
 
-def merge_comparisons(user: str, dsn: str, kvdbs: tuple, comparators: list,
-                      ranks: List[str], **kwargs):
-    logger.debug("collecting descriptions")
-    de_kvdb = compare_descriptions(user, dsn, kvdbs[0], **kwargs)
-
-    logger.debug("collecting terms")
-    te_kvdb = compare_terms(user, dsn, kvdbs[1], **kwargs)
-
-    logger.debug("collecting ranks")
-    ta_kvdb = compare_taxa(user, dsn, kvdbs[2], ranks, **kwargs)
-
-    logger.debug("collecting overlaps")
-    signatures, comparisons = merge_comparators(comparators, remove=False)
-
-    for acc_1, (n_seq, n_res) in signatures.items():
-        try:
-            n_desc, d_comp = d_kvdb[acc_1]
-        except KeyError:
-            n_desc = 0
-            d_comp = {}
-
-        try:
-            n_term, te_comp = te_kvdb[acc_1]
-        except KeyError:
-            n_term = 0
-            te_comp = {}
-
-        try:
-            n_ranks, ta_comp = ta_kvdb[acc_1]
-        except KeyError:
-            n_ranks = [0] * len(ranks)
-            ta_comp = {}
-        finally:
-            n_ranks = dict((zip(ranks, n_ranks)))
-
-        row1 = (acc_1, n_seq, n_res, n_desc, json.dumps(n_ranks), n_term)
-        rows = []
-        comp = comparisons[acc_1]
-        for acc_2, (n_colloc, n_prot_over, n_res_over) in comp.items():
-            n_desc = d_comp.get(acc_2, 0)
-            n_rank = dict(zip(ranks, ta_comp.get(acc_2, [0] * len(ranks))))
-            n_term = te_comp.get(acc_2, 0)
-            rows.append((acc_1, acc_2, n_colloc, n_prot_over, n_res, n_desc,
-                         json.dumps(n_rank), n_term))
-
-        yield row1, rows
-
-    for kvdb in (de_kvdb, te_kvdb, ta_kvdb):
-        kvdb.remove()
+# def merge_comparisons(user: str, dsn: str, kvdbs: tuple, comparators: list,
+#                       ranks: List[str], **kwargs):
+#     logger.debug("collecting descriptions")
+#     de_kvdb = compare_descriptions(user, dsn, kvdbs[0], **kwargs)
+#
+#     logger.debug("collecting terms")
+#     te_kvdb = compare_terms(user, dsn, kvdbs[1], **kwargs)
+#
+#     logger.debug("collecting ranks")
+#     ta_kvdb = compare_taxa(user, dsn, kvdbs[2], ranks, **kwargs)
+#
+#     logger.debug("collecting overlaps")
+#     signatures, comparisons = merge_comparators(comparators, remove=False)
+#
+#     for acc_1, (n_seq, n_res) in signatures.items():
+#         try:
+#             n_desc, d_comp = d_kvdb[acc_1]
+#         except KeyError:
+#             n_desc = 0
+#             d_comp = {}
+#
+#         try:
+#             n_term, te_comp = te_kvdb[acc_1]
+#         except KeyError:
+#             n_term = 0
+#             te_comp = {}
+#
+#         try:
+#             n_ranks, ta_comp = ta_kvdb[acc_1]
+#         except KeyError:
+#             n_ranks = [0] * len(ranks)
+#             ta_comp = {}
+#         finally:
+#             n_ranks = dict((zip(ranks, n_ranks)))
+#
+#         row1 = (acc_1, n_seq, n_res, n_desc, json.dumps(n_ranks), n_term)
+#         rows = []
+#         comp = comparisons[acc_1]
+#         for acc_2, (n_colloc, n_prot_over, n_res_over) in comp.items():
+#             n_desc = d_comp.get(acc_2, 0)
+#             n_rank = dict(zip(ranks, ta_comp.get(acc_2, [0] * len(ranks))))
+#             n_term = te_comp.get(acc_2, 0)
+#             rows.append((acc_1, acc_2, n_colloc, n_prot_over, n_res, n_desc,
+#                          json.dumps(n_rank), n_term))
+#
+#         yield row1, rows
+#
+#     for kvdb in (de_kvdb, te_kvdb, ta_kvdb):
+#         kvdb.remove()
