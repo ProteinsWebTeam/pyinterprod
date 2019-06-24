986a41dc758fb1224d6b05a889a8a8bb6bbf1d01
Matthias Blum <mat.blum@gmail.com>
2019-06-24 23:35:34 +0100

    removing dead code

--- pyinterprod/proteinupdate/proteins.py
+++ pyinterprod/proteinupdate/proteins.py
@@ -30,8 +30,20 @@ class ProteinDatabase(object):
             os.remove(self.path)
             self.temporary = True
 
-        self._create_table("protein_old")
-        self._create_table("protein_new")
+        with sqlite3.connect(self.path) as con:
+            con.execute(
+                """
+                CREATE TABLE IF NOT EXISTS protein (
+                  accession TEXT NOT NULL PRIMARY KEY,
+                  identifier TEXT NOT NULL,
+                  is_reviewed INTEGER NOT NULL,
+                  crc64 TEXT NOT NULL,
+                  length INTEGER NOT NULL,
+                  is_fragment INTEGER NOT NULL,
+                  taxon_id INTEGER NOT NULL
+                )
+                """
+            )
 
     def __del__(self):
         if self.temporary:
@@ -44,6 +56,11 @@ class ProteinDatabase(object):
         if self.temporary:
             self.drop()
 
+    def __iter__(self):
+        with sqlite3.connect(self.path) as con:
+            for row in con.execute("SELECT * FROM protein"):
+                yield row
+
     @property
     def size(self) -> int:
         return os.path.getsize(self.path)
@@ -54,154 +71,16 @@ class ProteinDatabase(object):
         except FileNotFoundError:
             pass
 
-    def _create_table(self, table_name: str):
-        with sqlite3.connect(self.path) as con:
-            con.execute(
-                """
-                CREATE TABLE IF NOT EXISTS {} (
-                  accession TEXT NOT NULL PRIMARY KEY,
-                  identifier TEXT NOT NULL,
-                  is_reviewed INTEGER NOT NULL,
-                  crc64 TEXT NOT NULL,
-                  length INTEGER NOT NULL,
-                  is_fragment INTEGER NOT NULL,
-                  taxon_id INTEGER NOT NULL
-                )
-                """.format(table_name)
-            )
-
-    def _insert(self, src: Generator, table_name: str, max_items: int) -> int:
-        query = """
-            INSERT INTO {}
-            VALUES (?, ?, ?, ?, ?, ?, ?)
-        """.format(table_name)
-
-        with sqlite3.connect(self.path) as con:
-            count = 0
-            items = []
-
-            for protein in src:
-                items.append(protein)
-                count += 1
-
-                if len(items) == max_items:
-                    con.executemany(query, items)
-                    items = []
-
-            if items:
-                con.executemany(query, items)
-            con.commit()
-
-        return count
-
-    def insert_new(self, src: Generator, max_items: int=10000) -> int:
-        return self._insert(src, "protein_new", max_items)
-
-    def insert_old(self, src: Generator, max_items: int=10000) -> int:
-        return self._insert(src, "protein_old", max_items)
-
-    def _iter(self, table_name: str) -> Generator[Tuple, None, None]:
-        with sqlite3.connect(self.path) as con:
-            for row in con.execute("SELECT * FROM {}".format(table_name)):
-                yield row
-
-    def iter_new(self) -> Generator[Tuple, None, None]:
-        return self._iter("protein_new")
-
-    def iter_old(self) -> Generator[Tuple, None, None]:
-        return self._iter("protein_old")
-
-    def get_deleted(self) -> Generator[str, None, None]:
-        with sqlite3.connect(self.path) as con:
-            cur = con.execute(
-                """
-                SELECT accession
-                FROM protein_old
-                EXCEPT
-                SELECT accession
-                FROM protein_new
-                """
-            )
-
-            for row in cur:
-                yield row[0]
-
-    def get_new(self) -> Generator[str, None, None]:
-        with sqlite3.connect(self.path) as con:
-            cur = con.execute(
-                """
-                SELECT
-                  accession,
-                  identifier,
-                  is_reviewed,
-                  crc64,
-                  length,
-                  is_fragment,
-                  taxon_id
-                FROM protein_new
-                WHERE accession NOT IN (
-                  SELECT accession
-                  FROM protein_old
-                )
-                """
-            )
-
-            for row in cur:
-                yield row
-
-    def get_annotation_changes(self) -> Generator[Tuple, None, None]:
-        with sqlite3.connect(self.path) as con:
-            cur = con.execute(
-                """
-                SELECT
-                  accession, p1.identifier, p1.is_reviewed, p1.crc64,
-                  p1.length, p1.is_fragment, p1.taxon_id
-                FROM protein_new AS p1
-                INNER JOIN protein_old AS p2
-                  USING (accession)
-                WHERE p1.crc64 = p2.crc64
-                AND (
-                     p1.identifier != p2.identifier
-                  OR p1.is_reviewed != p2.is_reviewed
-                  OR p1.crc64 != p2.crc64
-                  OR p1.length != p2.length
-                  OR p1.is_fragment != p2.is_fragment
-                  OR p1.taxon_id != p2.taxon_id
-                )
-
-                """
-            )
-
-            for row in cur:
-                yield row
-
-    def get_sequence_changes(self) -> Generator[Tuple, None, None]:
-        with sqlite3.connect(self.path) as con:
-            cur = con.execute(
-                """
-                SELECT
-                  accession, p1.identifier, p1.is_reviewed, p1.crc64,
-                  p1.length, p1.is_fragment, p1.taxon_id
-                FROM protein_new AS p1
-                INNER JOIN protein_old AS p2
-                  USING (accession)
-                WHERE p1.crc64 != p2.crc64
-                """
-            )
-
-            for row in cur:
-                yield row
-
 
 def load(user: str, dsn: str, swissprot_path: str, trembl_path: str,
          dir: Optional[str]=None):
 
     logger.info("loading proteins")
     with ProteinDatabase(dir=dir) as db:
-        count = sprot.load(swissprot_path, db.path, "protein_new")
+        count = sprot.load(swissprot_path, db.path, "protein")
         logger.info("New Swiss-Prot: {} proteins".format(count))
 
-        count = sprot.load(trembl_path, db.path, "protein_new")
+        count = sprot.load(trembl_path, db.path, "protein")
         logger.info("New TrEMBL: {} proteins".format(count))
 
         logger.info("disk space used: {:.0f} MB".format(db.size/1024**2))
@@ -222,38 +101,6 @@ def load(user: str, dsn: str, swissprot_path: str, trembl_path: str,
     logger.info("{} new proteins".format(_insert_new(con)))
     con.commit()
     con.close()
-
-
-def insert_new(user: str, dsn: str, swissprot_path: str, trembl_path: str,
-               dir: Optional[str]=None):
-    logger.info("loading proteins")
-    with ProteinDatabase(dir=dir) as db:
-        url = orautils.make_connect_string(user, dsn)
-
-        count = db.insert_old(_get_proteins(url))
-        logger.info("UniProt proteins in InterPro: {}".format(count))
-
-        count = sprot.load(swissprot_path, db.path, "protein_new")
-        logger.info("New Swiss-Prot: {} proteins".format(count))
-
-        count = sprot.load(trembl_path, db.path, "protein_new")
-        logger.info("New TrEMBL: {} proteins".format(count))
-
-        logger.info("disk space used: {:.0f} MB".format(db.size/1024**2))
-
-        con = cx_Oracle.connect(url)
-        _init_protein_changes(con)
-
-        count = _update_proteins(con, db)
-        logger.info("{} proteins updated".format(count))
-
-        count = _insert_proteins(con, db)
-        logger.info("{} proteins added".format(count))
-
-        count = _prepare_deletion(con, db)
-        logger.info("{} proteins to delete".format(count))
-
-        con.close()
 
 
 def _insert_all(con: cx_Oracle.Connection, db: ProteinDatabase) -> int:
@@ -280,7 +127,7 @@ def _insert_all(con: cx_Oracle.Connection, db: ProteinDatabase) -> int:
     """
     table = orautils.TablePopulator(con, query, autocommit=True)
 
-    for ac, name, is_rev, crc64, length, is_frag, taxid in db.iter_new():
+    for ac, name, is_rev, crc64, length, is_frag, taxid in db:
         table.insert((ac, name, 'S' if is_rev else 'T', crc64, length,
                      'Y' if is_frag else 'N', taxid))
 
@@ -494,13 +341,6 @@ def _delete_obsolete(user: str, dsn: str, truncate: bool=False):
     jobs = []
     for t in tables:
         to, tn, tc = t["owner"], t["name"], t["column"]
-
-        # if tn == "MATCH":
-        #     for p in orautils.get_partitions(cur, to, tn):
-        #         jobs.append((tn, tc, p["name"]))
-        # else:
-        #     jobs.append((tn, tc, None))
-
         partitions = orautils.get_partitions(cur, to, tn)
         if partitions:
             for p in partitions:
@@ -659,181 +499,6 @@ def check_crc64(user: str, dsn: str):
         logger.info("no CRC64 mismatches")
 
 
-def _get_proteins(url: str) -> Generator:
-    con = cx_Oracle.connect(url)
-    cur = con.cursor()
-    cur.execute(
-        """
-        SELECT
-          PROTEIN_AC, NAME, DBCODE, CRC64, LEN, FRAGMENT, TAX_ID
-        FROM INTERPRO.PROTEIN
-        """
-    )
-
-    for row in cur:
-        yield (
-            row[0],
-            row[1],
-            1 if row[2] == 'S' else 0,
-            row[3],
-            row[4],
-            1 if row[5] == 'Y' else 0,
-            row[6]
-        )
-
-    cur.close()
-    con.close()
-
-
-def _update_proteins(con: cx_Oracle.Connection, db: ProteinDatabase) -> int:
-    cur = con.cursor()
-
-    count = 0
-    for row in db.get_annotation_changes():
-        cur.execute(
-            """
-            UPDATE INTERPRO.PROTEIN
-            SET
-              NAME = :1,
-              DBCODE = :2,
-              CRC64 = :3,
-              LEN = :4,
-              FRAGMENT = :5,
-              TAX_ID = :6,
-              TIMESTAMP = SYSDATE,
-              USERSTAMP = USER
-              WHERE PROTEIN_AC = :7
-            """,
-            (
-                row[1],
-                'S' if row[2] else 'T',
-                row[3],
-                row[4],
-                'Y' if row[5] else 'N',
-                row[6],
-                row[0]
-            )
-        )
-        count += 1
-
-    query = """
-      INSERT INTO INTERPRO.PROTEIN_CHANGES (NEW_PROTEIN_AC)
-      VALUES (:1)
-    """
-    table = orautils.TablePopulator(con, query)
-    for row in db.get_sequence_changes():
-        cur.execute(
-            """
-            UPDATE INTERPRO.PROTEIN
-            SET
-              NAME = :1,
-              DBCODE = :2,
-              CRC64 = :3,
-              LEN = :4,
-              FRAGMENT = :5,
-              TAX_ID = :6,
-              TIMESTAMP = SYSDATE,
-              USERSTAMP = USER
-              WHERE PROTEIN_AC = :7
-            """,
-            (
-                row[1],
-                'S' if row[2] else 'T',
-                row[3],
-                row[4],
-                'Y' if row[5] else 'N',
-                row[6],
-                row[0]
-            )
-        )
-        count += 1
-        table.insert((row[0],))
-
-    table.close()
-    con.commit()
-    cur.close()
-    return count
-
-
-def _insert_proteins(con: cx_Oracle.Connection, db: ProteinDatabase) -> int:
-    # TIMESTAMP and USERSTAMP will be set to their DEFAULT
-    query = """
-        INSERT INTO INTERPRO.PROTEIN (
-          PROTEIN_AC, NAME, DBCODE, CRC64, LEN, FRAGMENT,
-          STRUCT_FLAG, TAX_ID
-        )
-        VALUES (:1, :2, :3, :4, :5, :6, 'N', :7)
-    """
-    table1 = orautils.TablePopulator(con, query)
-
-    query = """
-        INSERT INTO INTERPRO.PROTEIN_CHANGES (NEW_PROTEIN_AC)
-        VALUES (:1)
-    """
-    table2 = orautils.TablePopulator(con, query)
-
-    count = 0
-    for row in db.get_new():
-        table1.insert((
-            row[0],                     # accession
-            row[1],                     # identifier
-            'S' if row[2] else 'T',     # dbcode
-            row[3],                     # crc64
-            row[4],                     # length
-            'Y' if row[5] else 'N',     # sequence status (fragment)
-            row[6]                      # taxon ID
-        ))
-        table2.insert((row[0],))
-        count += 1
-
-    table1.close()
-    table2.close()
-    con.commit()
-    return count
-
-
-def _prepare_deletion(con: cx_Oracle.Connection, db: ProteinDatabase) -> int:
-    cur = con.cursor()
-    orautils.drop_table(cur, "INTERPRO", "PROTEIN_TO_DELETE")
-    cur.execute(
-        """
-        CREATE TABLE INTERPRO.PROTEIN_TO_DELETE
-        (
-            ID NUMBER NOT NULL,
-            PROTEIN_AC VARCHAR2(15) NOT NULL
-        ) NOLOGGING
-        """
-    )
-    cur.close()
-
-    query = """
-        INSERT INTO INTERPRO.PROTEIN_TO_DELETE
-        VALUES (:1, :2)
-    """
-    table = orautils.TablePopulator(con, query)
-
-    count = 0
-    for accession in db.get_deleted():
-        table.insert((count, accession))
-        count += 1
-
-    table.close()
-    con.commit()
-
-    cur = con.cursor()
-    orautils.gather_stats(cur, "INTERPRO", "PROTEIN_TO_DELETE")
-    cur.execute(
-        """
-        ALTER TABLE INTERPRO.PROTEIN_TO_DELETE
-        ADD CONSTRAINT PK_PROTEIN_TO_DELETE
-        PRIMARY KEY (ID)
-        """
-    )
-
-    cur.close()
-    return count
-
-
 def _count_proteins_to_delete(cur: cx_Oracle.Cursor) -> int:
     cur.execute(
         """
@@ -842,12 +507,6 @@ def _count_proteins_to_delete(cur: cx_Oracle.Cursor) -> int:
         """
     )
     return cur.fetchone()[0]
-
-
-def _init_protein_changes(con: cx_Oracle.Connection):
-    cur = con.cursor()
-    cur.execute("TRUNCATE TABLE INTERPRO.PROTEIN_CHANGES")
-    cur.close()
 
 
 def _delete_iter(url: str, table: str, column: str, stop: int, step: int,
