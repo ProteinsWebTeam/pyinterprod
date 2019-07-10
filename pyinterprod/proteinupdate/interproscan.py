11f2ab6dc556d575aaa155442754618cc73a24ad
Matthias Blum <mat.blum@gmail.com>
2019-07-10 11:50:00 +0100

    Renaming variable

--- pyinterprod/proteinupdate/interproscan.py
+++ pyinterprod/proteinupdate/interproscan.py
@@ -434,8 +434,8 @@ def _import_member_db(url: str, owner: str, table_src: str, table_dst: str,
                       partition: str, analysis_id: int, func: Callable):
     con = cx_Oracle.connect(url)
     cur = con.cursor()
-    dst_upi = _get_max_upi(cur, owner, table_src)
-    if not dst_upi or dst_upi != _get_max_upi(cur, owner, table_src, "ISPRO"):
+    upi = _get_max_upi(cur, owner, table_src)
+    if not upi or upi != _get_max_upi(cur, owner, table_src, "ISPRO"):
         # Not the same UPI: import table
         orautils.drop_mview(cur, owner, table_src)
         orautils.drop_table(cur, owner, table_src)
