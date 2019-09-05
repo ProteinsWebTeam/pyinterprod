46c135cc969fee7550d708ab17401a3aca2e0e06
Matthias Blum <mat.blum@gmail.com>
2019-09-05 12:16:48 +0100

    Pre-calculating predictions

--- pyinterprod/pronto/prediction.py
+++ pyinterprod/pronto/prediction.py
@@ -17,7 +17,7 @@ from .utils import Kvdb, PersistentBuffer as Buffer
 SIMILARITY_THRESHOLD = 0.75
 
 
-def _chunk_jobs(cur: cx_Oracle.Cursor, schema: str, chunk_size: int):
+def chunk_jobs(cur: cx_Oracle.Cursor, schema: str, chunk_size: int):
     cur.execute(
         f"""
         SELECT METHOD_AC
@@ -41,39 +41,34 @@ def _chunk_jobs(cur: cx_Oracle.Cursor, schema: str, chunk_size: int):
             yield row_start, row_stop, col_start, col_stop
 
 
-def _compare_chunk(database: str, start1: str, stop1: str, start2: str,
-                   stop2: str, dst: str, tmpdir: Optional[str]):
-    fd, _database = mkstemp(dir=tmpdir)
-    os.close(fd)
-    os.remove(_database)
-    shutil.copy(database, _database)
-
-    counts = {}
-    with Buffer(dir=tmpdir) as tmp, Buffer(filepath=dst) as buffer:
-        with Kvdb(_database) as kvdb:
-            for acc_1, values_1 in kvdb.range(start1, stop1):
-                counts[acc_1] = len(values_1)
-                intersections = {}
-
-                for acc_2, values_2 in kvdb.range(start2, stop2):
-                    if acc_1 == acc_2:
-                        continue  # skip diagonal
-
+def _compare_chunk(database: str, start: str, stop: str, task_queue: Queue,
+                   done_queue: Queue):
+    with Kvdb(database) as kvdb:
+        for acc_1, values_1 in iter(task_queue.get, None):
+            intersections = {}
+            for acc_2, values_2 in kvdb.range(start, stop):
+                if acc_1 < acc_2:
                     intersection = len(values_1 & values_2)
                     if intersection:
                         intersections[acc_2] = intersection
 
-                if intersections:
-                    tmp.add((acc_1, intersections))
+            if intersections:
+                done_queue.put((acc_1, intersections))
 
-            if start1 != start2:
-                for acc_1, values_1 in kvdb.range(start2, stop2):
-                    counts[acc_1] = len(values_1)
 
-        size = kvdb.size + tmp.size
-        kvdb.remove()
+def _persist_comparisons(database: str, start1: str, stop1: str, start2: str,
+                         stop2: str, dst: str, queue: Queue):
+    with Kvdb(database) as kvdb:
+        counts = {}
+        for acc, values in kvdb.range(start1, stop1):
+            counts[acc] = len(values)
 
-        for acc_1, intersections in tmp:
+        if start2 != start1:
+            for acc, values in kvdb.range(start2, stop2):
+                counts[acc] = len(values)
+
+    with Buffer(filepath=dst) as buffer:
+        for acc_1, intersections in iter(queue.get, None):
             cnt_1 = counts[acc_1]
             similarities = {}
 
@@ -92,19 +87,54 @@ def _compare_chunk(database: str, start1: str, stop1: str, start2: str,
 
             buffer.add((acc_1, similarities))
 
-        tmp.remove()
 
-    logger.info(f"disk usage: {size/1024**2:.0f}MB")
+def compare_chunk(database: str, start1: str, stop1: str, start2: str,
+                  stop2: str, dst: str, processes: int=8,
+                  tmpdir: Optional[str]=None):
+    fd, _database = mkstemp(dir=tmpdir)
+    os.close(fd)
+    os.remove(_database)
+    shutil.copy(database, _database)
+    database = _database
+
+    task_queue = Queue(maxsize=1)
+    done_queue = Queue()
+
+    workers = []
+    for _ in range(max(1, processes-2)):
+        p = Process(target=_compare_chunk,
+                    args=(database, start2, stop2, task_queue, done_queue))
+        p.start()
+        workers.append(p)
+
+    persister = Process(target=_persist_comparisons,
+                        args=(database, start1, stop1, start2, stop2, dst,
+                              done_queue))
+    persister.start()
+
+    with Kvdb(database) as kvdb:
+        for acc, values in kvdb.range(start1, stop1):
+            task_queue.put((acc, values))
+
+    for _ in workers:
+        task_queue.put(None)
+
+    for p in workers:
+        p.join()
+
+    done_queue.put(None)
+    persister.join()
+    os.remove(database)
 
 
-def _compare_signatures(user: str, dsn: str, query: str, outdir: str,
-                        queue: Queue, chunk_size: int, tmpdir: Optional[str],
-                        job_queue: Optional[str]):
+def compare_signatures(user: str, dsn: str, query: str, outdir: str,
+                       queue: Queue, chunk_size: int, tmpdir: Optional[str],
+                       job_queue: Optional[str]):
     con = cx_Oracle.connect(orautils.make_connect_string(user, dsn))
     cur = con.cursor()
 
     # Export signatures to local database
-    fd, database = mkstemp(suffix=".db", dir=tmpdir)
+    fd, database = mkstemp(dir=tmpdir)
     os.close(fd)
     os.remove(database)
     with Kvdb(database, insertonly=True) as kvdb:
@@ -138,35 +168,39 @@ def _compare_signatures(user: str, dsn: str, query: str, outdir: str,
     # Submit all jobs
     owner = user.split('/')[0]
     tasks = {}
-    for start1, stop1, start2, stop2 in _chunk_jobs(cur, owner, chunk_size):
+    for start1, stop1, start2, stop2 in chunk_jobs(cur, owner, chunk_size):
         fd, filepath = mkstemp(dir=outdir)
         os.close(fd)
         t = Task(
-            fn=_compare_chunk,
-            args=(database, start1, stop1, start2, stop2, filepath, tmpdir),
-            scheduler=dict(queue=job_queue, mem=4000)
+            fn=compare_chunk,
+            args=(database, start1, stop1, start2, stop2, filepath),
+            kwargs=dict(processes=8, tmpdir=tmpdir),
+            scheduler=dict(queue=job_queue, cpu=8, mem=4000)
         )
         t.run(workdir=outdir)
         tasks[t] = (start1, stop1, start2, stop2, filepath)
 
     cur.close()
     con.close()
-    running = len(tasks)
-    while running:
+    while tasks:
+        _tasks = {}
         for t in tasks:
             if t.done():
                 start1, stop1, start2, stop2, filepath = tasks[t]
-                running -= 1
 
                 logger.debug(f"{t.stdout}\n{t.stderr}")
                 if t.successful():
-                    queue.put((query, start1, stop1, start2, stop2, filepath))
+                    queue.put((query, start1, start2, filepath))
                 else:
-                    queue.put((None, start1, stop1, start2, stop2, filepath))
+                    queue.put((None, start1, start2, filepath))
+            else:
+                _tasks[t] = tasks[t]
 
+        tasks = _tasks
         time.sleep(10)
 
     queue.put(None)
+    os.remove(database)
 
 
 def compare(user: str, dsn: str, outdir: str, chunk_size: int=10000,
@@ -191,7 +225,7 @@ def compare(user: str, dsn: str, outdir: str, chunk_size: int=10000,
         ORDER BY METHOD_AC
     """
     sources[query] = "desc"
-    producers.append(Process(target=_compare_signatures,
+    producers.append(Process(target=compare_signatures,
                              args=(user, dsn, query, outdir, queue,
                                    chunk_size, job_tmpdir, job_queue)))
 
@@ -201,7 +235,7 @@ def compare(user: str, dsn: str, outdir: str, chunk_size: int=10000,
         ORDER BY METHOD_AC
     """
     sources[query] = "taxa"
-    producers.append(Process(target=_compare_signatures,
+    producers.append(Process(target=compare_signatures,
                              args=(user, dsn, query, outdir, queue,
                                    chunk_size, job_tmpdir, job_queue)))
 
@@ -219,131 +253,191 @@ def compare(user: str, dsn: str, outdir: str, chunk_size: int=10000,
         ORDER BY METHOD_AC
     """
     sources[query] = "terms"
-    producers.append(Process(target=_compare_signatures,
+    producers.append(Process(target=compare_signatures,
                              args=(user, dsn, query, outdir, queue,
                                    chunk_size, job_tmpdir, job_queue)))
 
     for p in producers:
         p.start()
 
-    buffer = Buffer(os.path.join(outdir, "results"))
-
+    con = cx_Oracle.connect(orautils.make_connect_string(user, dsn))
+    predictor = Predictor(con, owner)
     running = len(producers)
     chunks = {}
     failed = 0
     while running:
-        for query, start1, stop1, start2, stop2, filepath in iter(queue.get, None):
+        for query, start1, start2, filepath in iter(queue.get, None):
             if query is None:
                 failed += 1
                 continue
 
-            source = sources[query]
-            key = (start1, stop1, start2, stop2)
-            try:
-                c = chunks[key]
-            except KeyError:
-                c = chunks[key] = {
-                    "desc": None,
-                    "taxa": None,
-                    "term": None
-                }
+            key = (start1, start2)
+            if key not in chunks:
+                chunks[key] = {k: None for k in sources.values()}
 
-            c[source] = filepath
-            if all(c.values()):
-                buffer.add((start1, stop1, start2, stop2, c))
+            source = sources[query]
+            chunks[key][source] = filepath
+
+            if all(chunks[key].values()):
+                predictor.put(chunks.pop(key))
 
         running -= 1
 
     for p in producers:
         p.join()
 
-    logger.warning(f"{failed} jobs failed")
+    predictor.close()
 
-    return
+    if failed:
+        con.close()
+        raise RuntimeError(f"{failed} jobs failed")
 
-    logger.info("indexing/anayzing METHOD_SIMILARITY")
-    owner = user.split('/')[0]
-    con = cx_Oracle.connect(orautils.make_connect_string(user, dsn))
-    cur = con.cursor()
-    orautils.grant(cur, owner, "METHOD_SIMILARITY", "SELECT", "INTERPRO_SELECT")
-    orautils.drop_index(cur, owner, "I_METHOD_SIMILARITY$AC1")
-    orautils.drop_index(cur, owner, "I_METHOD_SIMILARITY$AC2")
-    cur.execute(
-        """
-        CREATE INDEX I_METHOD_SIMILARITY$AC1
-        ON {}.METHOD_SIMILARITY (METHOD_AC1) NOLOGGING
-        """.format(owner)
-    )
-    cur.execute(
-        """
-        CREATE INDEX I_METHOD_SIMILARITY$AC2
-        ON {}.METHOD_SIMILARITY (METHOD_AC2) NOLOGGING
-        """.format(owner)
-    )
-    orautils.gather_stats(cur, owner, "METHOD_SIMILARITY")
-    cur.close()
+    predictor.index()
     con.close()
-    logger.info("METHOD_SIMILARITY is ready")
 
 
 class Predictor(object):
-    def __init__(self, user: str, dsn: str):
-        owner = user.split('/')[0]
-        self.con = cx_Oracle.connect(orautils.make_connect_string(user, dsn))
-        cur = self.con.cursor()
+    def __init__(self, con: cx_Oracle.Connection, owner: str):
+        self.con = con
+        self.owner = owner
+
+        cur = con.cursor()
         orautils.drop_table(cur, owner, "METHOD_SIMILARITY", purge=True)
         cur.execute(
             f"""
             CREATE TABLE {owner}.METHOD_SIMILARITY
             (
                 METHOD_AC1 VARCHAR2(25) NOT NULL,
-                PROT_COUNT1 NUMBER(*) NOT NULL,
                 METHOD_AC2 VARCHAR2(25) NOT NULL,
+                PROT_COUNT1 NUMBER(*) NOT NULL,
                 PROT_COUNT2 NUMBER(*) NOT NULL,
                 COLL_COUNT NUMBER(*) NOT NULL,
                 PROT_OVER_COUNT NUMBER(*) NOT NULL,
                 PROT_PRED CHAR(1) NOT NULL,
-                DESC_PRED CHAR(1) NOT NULL,
-                TAXA_PRED CHAR(1) NOT NULL,
-                TERM_PRED CHAR(1) NOT NULL
+                RESI_PRED CHAR(1) NOT NULL,
+                DESC_PRED CHAR(1),
+                TAXA_PRED CHAR(1),
+                TERM_PRED CHAR(1)
             ) NOLOGGING
             """
         )
-        cur.close()
 
-        query = f"""
-            INSERT /*+ APPEND */ INTO {owner}.METHOD_SIMILARITY
-            VALUES (;1, :2, :3, :4, :5, :6, :7, :8, :9, :10)
-        """
-        self.table = orautils.TablePopulator(self.con, query, autocommit=True)
-
-        self.query = f"""
+        cur.execute(
+            f"""
             SELECT
               METHOD_AC1, METHOD_AC2, PROT_COUNT1, RES_COUNT1, PROT_COUNT2,
               RES_COUNT2, COLL_COUNT, PROT_OVER_COUNT, RES_OVER_COUNT
             FROM {owner}.METHOD_OVERLAP
-            WHERE METHOD_AC1 BETWEEN :1 AND :2
-              AND METHOD_AC2 BETWEEN :3 AND :4
-        """
-
-    def digest(self, start1: str, stop1: str, start2: str, stop2: str, chunk: Dict[str, str]):
-        cur = self.con.cursor()
-        cur.execute(self.query, (start1, stop1, start2, stop2))
-
-        overlaps = {}
+            """
+        )
+        self.predictions = {}
         for row in cur:
             acc_1 = row[0]
             acc_2 = row[1]
+            prot_cnt_1 = row[2]
+            res_cnt_1 = row[3]
+            prot_cnt_2 = row[4]
+            res_cnt_2 = row[5]
+            coll_cnt = row[6]
+            prot_over_cnt = row[7]
+            res_over_cnt = row[8]
 
-            if acc_1 in overlaps:
-                overlaps[acc_1][acc_2] = row[2:]
-            else:
-                overlaps[acc_1] = {acc_2: row[2:]}
+            # Protein similarity
+            psim = prot_over_cnt / (prot_cnt_1 + prot_cnt_2 - prot_over_cnt)
+            pct1 = prot_over_cnt / prot_cnt_1
+            pct2 = prot_over_cnt / prot_cnt_2
 
+            # Residue similarity
+            rsim = res_over_cnt / (res_cnt_1 + res_cnt_2 - res_over_cnt)
+            rct1 = res_over_cnt / res_cnt_1
+            rct2 = res_over_cnt / res_cnt_2
+
+            # Protein/residue values to persist
+            obj = [prot_cnt_1, prot_cnt_2, coll_cnt, prot_over_cnt,
+                   self.predict(psim, pct1, pct2),
+                   self.predict(rsim, rct1, rct2)]
+
+            try:
+                self.predictions[acc_1][acc_2] = obj
+            except KeyError:
+                self.predictions[acc_1] = {acc_2: obj}
         cur.close()
 
-        data = {}
+        query = f"""
+            INSERT /*+ APPEND */ INTO {owner}.METHOD_SIMILARITY
+            VALUES (:1, :2, :3, :4, :5, :6, :7, :8, :9, :10, :11)
+        """
+        self.table = orautils.TablePopulator(con, query, autocommit=True)
+
+    def __del__(self):
+        self.close()
+
+    def close(self):
+        self.table.close()
+
+    def index(self):
+        cur = self.con.cursor()
+        orautils.grant(cur, self.owner, "METHOD_SIMILARITY", "SELECT", "INTERPRO_SELECT")
+        cur.execute(
+            f"""
+            CREATE INDEX I_METHOD_SIMILARITY$AC1
+            ON {self.owner}.METHOD_SIMILARITY (METHOD_AC1) NOLOGGING
+            """
+        )
+        cur.execute(
+            f"""
+            CREATE INDEX I_METHOD_SIMILARITY$AC2
+            ON {self.owner}.METHOD_SIMILARITY (METHOD_AC2) NOLOGGING
+            """
+        )
+        cur.close()
+
+    def put(self, chunk: Dict[str, str]):
+        predictions = {}
         for source, filepath in chunk.items():
+            if source == "desc":
+                i = 8
+            elif source == "taxa":
+                i = 9
+            else:
+                i = 10
+
             with Buffer(filepath) as buffer:
                 for acc_1, similarities in buffer:
-                    pass
+                    for acc_2, (sim, ct1, ct2) in similarities.items():
+                        try:
+                            obj = self.predictions[acc_1][acc_2]
+                        except KeyError:
+                            continue  # not a single common protein: skip
+
+                        if acc_1 in predictions:
+                            try:
+                                row = predictions[acc_1][acc_2]
+                            except KeyError:
+                                row = [acc_1, acc_2] + obj + [None, None, None]
+                                predictions[acc_1][acc_2] = row
+                        else:
+                            row = [acc_1, acc_2] + obj + [None, None, None]
+                            predictions[acc_1] = {acc_2: row}
+
+                        row[i] = self.predict(sim, ct1, ct2)
+
+                buffer.remove()
+
+        for acc_1 in predictions:
+            for acc_2, row in predictions[acc_1].items():
+                self.table.insert(tuple(row))
+
+    @staticmethod
+    def predict(similarity: float, containment1: float, containment2: float):
+        if similarity >= SIMILARITY_THRESHOLD:
+            return 'S'  # Similar
+        elif containment1 >= SIMILARITY_THRESHOLD:
+            if containment2 >= SIMILARITY_THRESHOLD:
+                return 'R'  # Related
+            else:
+                return 'C'  # Child
+        elif containment2 >= SIMILARITY_THRESHOLD:
+            return 'P'  # Parent
+        else:
+            return 'D'  # Dissimilar
