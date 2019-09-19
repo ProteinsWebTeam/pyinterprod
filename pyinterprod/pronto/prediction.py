c621604722d205e1f58323cbbaef73d74d4c48eb
Matthias Blum <mat.blum@gmail.com>
2019-09-19 13:48:52 +0100

    Adding another index

--- pyinterprod/pronto/prediction.py
+++ pyinterprod/pronto/prediction.py
@@ -354,6 +354,12 @@ def compare(user: str, dsn: str, outdir: str, chunk_size: int=10000,
         ON {owner}.METHOD_SIMILARITY (DBCODE1, PROT_PRED) NOLOGGING
         """
     )
+    cur.execute(
+        f"""
+        CREATE INDEX I_METHOD_SIMILARITY$AC$PRED 
+        ON {owner}.METHOD_SIMILARITY(METHOD_AC1, PROT_PRED) NOLOGGING
+        """
+    )
 
     cur.execute(f"UPDATE {owner}.CV_DATABASE SET IS_READY = 'Y'")
     cur.close()
