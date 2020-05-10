cede47c9b674ee4d5ddc834110891474b54e3ba1
Matthias Blum <mat.blum@gmail.com>
2020-05-10 09:49:56 +0100

    Skipping MV_TAX_ENTRY_COUNT update

--- pyinterprod/interpro/taxonomy.py
+++ pyinterprod/interpro/taxonomy.py
@@ -86,48 +86,48 @@ def refresh_taxonomy(url: str):
     con.commit()
     oracle.gather_stats(cur, "INTERPRO", "UNIPROT_TAXONOMY")
 
-    logger.info("populating MV_TAX_ENTRY_COUNT")
-    oracle.truncate_table(cur, "INTERPRO.MV_TAX_ENTRY_COUNT", reuse_storage=True)
-    cur.execute(
-        """
-        INSERT INTO  INTERPRO.MV_TAX_ENTRY_COUNT
-        WITH QUERY1 AS (
-          SELECT ENTRY_AC, ANC.PARENT AS TAX_ID, COUNT(1) AS COUNT
-          FROM INTERPRO.UNIPROT_TAXONOMY UT
-          JOIN INTERPRO.MV_ENTRY2PROTEIN_TRUE MVEP
-            ON UT.PROTEIN_AC=MVEP.PROTEIN_AC
-          JOIN (
-            SELECT NVL(
-                     SUBSTR(
-                       SYS_CONNECT_BY_PATH(TAX_ID, '.'), 
-                       2, 
-                       INSTR(SYS_CONNECT_BY_PATH (TAX_ID,'.'),'.',2) - 2
-                     ), 
-                     TAX_ID
-                   ) AS CHILD, 
-                   TAX_ID AS PARENT
-            FROM INTERPRO.ETAXI ET
-            CONNECT BY PRIOR PARENT_ID=TAX_ID
-          ) ANC ON ANC.CHILD=UT.TAX_ID
-          GROUP BY ENTRY_AC, ANC.PARENT
-        ), 
-        QUERY2 AS (
-          SELECT ENTRY_AC, TAX_ID, COUNT(1) AS COUNT
-          FROM INTERPRO.UNIPROT_TAXONOMY UT
-          JOIN INTERPRO.MV_ENTRY2PROTEIN_TRUE MVEP
-          ON UT.PROTEIN_AC=MVEP.PROTEIN_AC
-          GROUP BY ENTRY_AC, TAX_ID
-        )
-        SELECT QUERY1.ENTRY_AC, QUERY1.TAX_ID, QUERY1.COUNT AS COUNT, 
-               QUERY2.COUNT AS COUNT_SPECIFIED_TAX_ID
-        FROM QUERY1
-        LEFT OUTER JOIN QUERY2
-          ON QUERY1.ENTRY_AC = QUERY2.ENTRY_AC 
-          AND QUERY1.TAX_ID = QUERY2.TAX_ID
-        """
-    )
-    con.commit()
-    oracle.gather_stats(cur, "INTERPRO", "MV_TAX_ENTRY_COUNT")
+    # logger.info("populating MV_TAX_ENTRY_COUNT")
+    # oracle.truncate_table(cur, "INTERPRO.MV_TAX_ENTRY_COUNT", reuse_storage=True)
+    # cur.execute(
+    #     """
+    #     INSERT INTO  INTERPRO.MV_TAX_ENTRY_COUNT
+    #     WITH QUERY1 AS (
+    #       SELECT ENTRY_AC, ANC.PARENT AS TAX_ID, COUNT(1) AS COUNT
+    #       FROM INTERPRO.UNIPROT_TAXONOMY UT
+    #       JOIN INTERPRO.MV_ENTRY2PROTEIN_TRUE MVEP
+    #         ON UT.PROTEIN_AC=MVEP.PROTEIN_AC
+    #       JOIN (
+    #         SELECT NVL(
+    #                  SUBSTR(
+    #                    SYS_CONNECT_BY_PATH(TAX_ID, '.'),
+    #                    2,
+    #                    INSTR(SYS_CONNECT_BY_PATH (TAX_ID,'.'),'.',2) - 2
+    #                  ),
+    #                  TAX_ID
+    #                ) AS CHILD,
+    #                TAX_ID AS PARENT
+    #         FROM INTERPRO.ETAXI ET
+    #         CONNECT BY PRIOR PARENT_ID=TAX_ID
+    #       ) ANC ON ANC.CHILD=UT.TAX_ID
+    #       GROUP BY ENTRY_AC, ANC.PARENT
+    #     ),
+    #     QUERY2 AS (
+    #       SELECT ENTRY_AC, TAX_ID, COUNT(1) AS COUNT
+    #       FROM INTERPRO.UNIPROT_TAXONOMY UT
+    #       JOIN INTERPRO.MV_ENTRY2PROTEIN_TRUE MVEP
+    #       ON UT.PROTEIN_AC=MVEP.PROTEIN_AC
+    #       GROUP BY ENTRY_AC, TAX_ID
+    #     )
+    #     SELECT QUERY1.ENTRY_AC, QUERY1.TAX_ID, QUERY1.COUNT AS COUNT,
+    #            QUERY2.COUNT AS COUNT_SPECIFIED_TAX_ID
+    #     FROM QUERY1
+    #     LEFT OUTER JOIN QUERY2
+    #       ON QUERY1.ENTRY_AC = QUERY2.ENTRY_AC
+    #       AND QUERY1.TAX_ID = QUERY2.TAX_ID
+    #     """
+    # )
+    # con.commit()
+    # oracle.gather_stats(cur, "INTERPRO", "MV_TAX_ENTRY_COUNT")
 
     cur.close()
     con.close()
