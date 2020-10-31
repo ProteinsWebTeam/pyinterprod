de7ab2f736f7226e0311b7575cde4f5686968f2f
Matthias Blum <mat.blum@gmail.com>
2020-10-31 14:37:16 +0000

    Using XML file to get CDD signatures

--- pyinterprod/interpro/contrib/cdd.py
+++ pyinterprod/interpro/contrib/cdd.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-import re
 from typing import List
+import xml.etree.ElementTree as ET
 
 from .model import Method
 
@@ -10,21 +10,25 @@ _TYPE = 'D'
 
 def parse_signatures(filepath: str) -> List[Method]:
     """
-    Parse the cddid.tbl file distributed with CDD releases
+    Parse the cdd_interpro.xml file provided by CDD
+    As of CDD 3.18, the file is available here:
+        ftp://ftp.ncbi.nih.gov/pub/mmdb/cdd/cdd_interpro.xml.gz
 
     :param filepath:
     :return:
     """
     signatures = []
-    with open(filepath, "rt") as fh:
-        for line in fh:
-            values = line.rstrip().split('\t')
-            pssm_id, acc, name, descr, pssm_length = values
-            if re.match(r"cd\d+$", acc):
-                signatures.append(Method(accession=acc,
-                                         sig_type=_TYPE,
-                                         name=name.replace(',', '_'),
-                                         description=name,
-                                         abstract=descr))
+
+    tree = ET.parse(filepath)
+    root = tree.getroot()
+    namespace = "{http://www.ebi.ac.uk/schema/interpro}"
+    for sig in root.findall(f"{namespace}signature"):
+        attrib = sig.attrib
+        abstract = sig.find(f"{namespace}abstract").text.strip()
+        signatures.append(Method(accession=attrib["ac"],
+                                 sig_type=_TYPE,
+                                 name=attrib["name"].replace(',', '_'),
+                                 description=attrib["desc"],
+                                 abstract=abstract))
 
     return signatures
