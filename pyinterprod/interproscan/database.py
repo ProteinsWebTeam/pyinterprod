c8d1667eb9865377cf11abfd2f0a91cb85c2795b
Matthias Blum <mat.blum@gmail.com>
2022-02-09 17:16:40 +0000

    Add function to import proteins from UniParc database

--- pyinterprod/interproscan/database.py
+++ pyinterprod/interproscan/database.py
@@ -1,4 +1,4 @@
-from typing import Sequence
+from typing import Optional, Sequence
 
 import cx_Oracle
 
@@ -68,19 +68,19 @@ def init_tables(ippro_uri: str, ispro_uri: str, i5_dir: str, others=None):
     )
     hwm = dict(cur.fetchall())
 
-    for table in ("IPM2_JOBS", "IPM2_ANALYSIS", "IPM2_TABLES"):
+    for table in ("ANALYSIS_JOBS", "ANALYSIS", "ANALYSIS_TABLES"):
         oracle.drop_table(cur, f"IPRSCAN.{table}", purge=True)
 
     cur.execute(
         """
-        CREATE TABLE IPRSCAN.IPM2_ANALYSIS
+        CREATE TABLE IPRSCAN.ANALYSIS
         (
             ID NUMBER(4) NOT NULL
-                CONSTRAINT IPM2_ANALYSIS_PK PRIMARY KEY,
+                CONSTRAINT ANALYSIS_PK PRIMARY KEY,
             NAME VARCHAR2(30) NOT NULL,
             VERSION VARCHAR2(20) NOT NULL,
             ACTIVE NUMBER(1)
-                CONSTRAINT IPM2_ANALYSIS_CHK
+                CONSTRAINT ANALYSIS_CHK
                 CHECK (ACTIVE in (0, 1)),
             MAX_UPI VARCHAR2(13) DEFAULT NULL,
             I5_DIR VARCHAR2(1000) NOT NULL,
@@ -91,7 +91,7 @@ def init_tables(ippro_uri: str, ispro_uri: str, i5_dir: str, others=None):
 
     cur.execute(
         """
-        CREATE TABLE IPRSCAN.IPM2_TABLES
+        CREATE TABLE IPRSCAN.ANALYSIS_TABLES
         (
             NAME VARCHAR2(30) NOT NULL PRIMARY KEY,
             MATCH_TABLE VARCHAR2(30) NOT NULL,
@@ -102,17 +102,17 @@ def init_tables(ippro_uri: str, ispro_uri: str, i5_dir: str, others=None):
 
     cur.execute(
         """
-        CREATE TABLE IPRSCAN.IPM2_JOBS
+        CREATE TABLE IPRSCAN.ANALYSIS_JOBS
         (
             ANALYSIS_ID NUMBER(4) NOT NULL
-                CONSTRAINT IPM2_JOBS_FK
-                REFERENCES IPM2_ANALYSIS (ID),
+                CONSTRAINT ANALYSIS_JOBS_FK
+                REFERENCES ANALYSIS (ID),
             UPI_FROM VARCHAR2(13) NOT NULL,
             UPI_TO VARCHAR2(13) NOT NULL,
             START_TIME DATE DEFAULT NULL,
             END_TIME DATE DEFAULT NULL,
             MAX_MEMORY NUMBER(6) DEFAULT NULL,
-            CONSTRAINT IPM2_JOBS_PK
+            CONSTRAINT ANALYSIS_JOBS_PK
                 PRIMARY KEY (ANALYSIS_ID, UPI_FROM, UPI_TO)
         )
         """
@@ -121,7 +121,7 @@ def init_tables(ippro_uri: str, ispro_uri: str, i5_dir: str, others=None):
     for analysis_id, (name, version, i5_dir) in active_analyses.items():
         cur.execute(
             """
-            INSERT INTO IPRSCAN.IPM2_ANALYSIS
+            INSERT INTO IPRSCAN.ANALYSIS
                 (ID, NAME, VERSION, ACTIVE, MAX_UPI, I5_DIR)
             VALUES
                 (:1, :2, :3, :4, :5, :6)
@@ -131,7 +131,7 @@ def init_tables(ippro_uri: str, ispro_uri: str, i5_dir: str, others=None):
 
     cur.executemany(
         """
-        INSERT INTO IPRSCAN.IPM2_TABLES
+        INSERT INTO IPRSCAN.ANALYSIS_TABLES
         VALUES (:1, :2, :3)
         """,
         tables
@@ -147,8 +147,8 @@ def get_analyses(cur: cx_Oracle.Cursor) -> dict:
         """
         SELECT A.ID, A.NAME, A.VERSION, A.MAX_UPI, A.I5_DIR,
                T.MATCH_TABLE, T.SITE_TABLE
-        FROM IPRSCAN.IPM2_ANALYSIS A
-        INNER JOIN IPRSCAN.IPM2_TABLES T
+        FROM IPRSCAN.ANALYSIS A
+        INNER JOIN IPRSCAN.ANALYSIS_TABLES T
             ON A.NAME = T.NAME
         WHERE A.ACTIVE = 1
         """
@@ -217,7 +217,7 @@ def reset_max_upi(uri: str, analyses: Sequence[int]):
     for analysis_id in analyses:
         cur.execute(
             """
-            UPDATE IPRSCAN.IPM2_ANALYSIS
+            UPDATE IPRSCAN.ANALYSIS
             SET MAX_UPI = NULL
             WHERE ID = :1
             """,
@@ -225,5 +225,84 @@ def reset_max_upi(uri: str, analyses: Sequence[int]):
         )
 
     con.commit()
+    cur.close()
+    con.close()
+
+
+def iter_proteins(uri: str, greather_than: Optional[str] = None):
+    con = cx_Oracle.connect(uri)
+    cur = con.cursor()
+    cur.outputtypehandler = oracle.clob_as_str
+
+    sql = """
+        SELECT ID, UPI, TIMESTAMP, USERSTAMP, CRC64, LEN, SEQ_SHORT, 
+               SEQ_LONG, MD5
+        FROM UNIPARC.PROTEIN      
+    """
+
+    if greather_than is not None:
+        sql += "WHERE UPI > :upi"
+        params = {"upi": greather_than}
+    else:
+        params = {}
+
+    cur.execute(sql, params)
+    yield from cur
+    cur.close()
+    con.close()
+
+
+def import_uniparc(ispro_uri: str, uniparc_uri: str, top_up: bool = False):
+    con = cx_Oracle.connect(ispro_uri)
+    cur = con.cursor()
+
+    if top_up:
+        cur.execute("SELECT MAX(UPI) FROM UNIPARC.PROTEIN")
+        max_upi, = cur.fetchone()
+    else:
+        max_upi = None
+        oracle.drop_mview(cur, "UNIPARC.PROTEIN")
+        oracle.drop_table(cur, "UNIPARC.PROTEIN", purge=True)
+        cur.execute(
+            """
+                CREATE TABLE UNIPARC.PROTEIN
+                (
+                    ID NUMBER(15) NOT NULL,
+                    UPI CHAR(13) NOT NULL,
+                    TIMESTAMP DATE NOT NULL,
+                    USERSTAMP VARCHAR2(30) NOT NULL,
+                    CRC64 CHAR(16) NOT NULL,
+                    LEN NUMBER(6) NOT NULL,
+                    SEQ_SHORT VARCHAR2(4000),
+                    SEQ_LONG CLOB,
+                    MD5 VARCHAR2(32) NOT NULL
+                ) NOLOGGING
+            """
+        )
+
+    records = []
+    req = """
+        INSERT /*+ APPEND */ 
+        INTO UNIPARC.PROTEIN
+        VALUES (:1, :2, :3, :4, :5, :6, :7, :8, :9)
+    """
+
+    for rec in iter_proteins(uniparc_uri, greather_than=max_upi):
+        records.append(rec)
+
+        if len(records) == 1000:
+            cur.executemany(req, records)
+            con.commit()
+            records.clear()
+
+    if records:
+        cur.executemany(req, records)
+        con.commit()
+        records.clear()
+
+    if not top_up:
+        cur.execute(f"GRANT SELECT ON UNIPARC.PROTEIN TO PUBLIC")
+        cur.execute("CREATE UNIQUE INDEX PK_PROTEIN ON UNIPARC.PROTEIN (UPI)")
+
     cur.close()
     con.close()
