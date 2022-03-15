bc3b96ecbe656099ed82520118db9723d6f7fd69
typhainepl <typhaine@ebi.ac.uk>
2022-03-15 16:54:24 +0000

    add antifam to Pronto

--- pyinterprod/pronto/match.py
+++ pyinterprod/pronto/match.py
@@ -64,6 +64,11 @@ class MatchIterator:
                    M.POS_FROM, M.POS_TO, M.FRAGMENTS
             FROM INTERPRO.MATCH M
             INNER JOIN INTERPRO.CV_DATABASE D ON M.DBCODE = D.DBCODE
+            UNION ALL
+            SELECT FM.PROTEIN_AC, FM.METHOD_AC, LOWER(D.DBSHORT), FM.POS_FROM, FM.POS_TO, NULL
+            FROM INTERPRO.FEATURE_MATCH FM
+            INNER JOIN INTERPRO.CV_DATABASE D ON FM.DBCODE = D.DBCODE
+            WHERE FM.DBCODE = 'a'
             """
         )
 
