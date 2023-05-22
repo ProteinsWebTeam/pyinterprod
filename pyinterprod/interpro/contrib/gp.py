d258648325a556542d161af592851d42de739d3b
laisecf <laisecflorentino@gmail.com>
2023-05-22 17:05:12 +0100

    rename file

--- pyinterprod/interpro/contrib/gp.py
+++ pyinterprod/interpro/contrib/gp.py
@@ -5,14 +5,14 @@ def import_gp(cur: cx_Oracle.Cursor, file_path: str):
     gp = []
     with open(file_path, "rt") as fh:
         for line in fh:
-            if line.startswith('AC'):
+            if line.startswith("AC"):
                 entry_ac = line.split()[1]
-            elif line.startswith('DE'):
+            elif line.startswith("DE"):
                 description = line.split(" ", maxsplit=1)[1].strip()
-            elif line.startswith('EV'):
-                ev = line.split()[1].split(';')[0]
-                if ev.startswith('IPR'):
-                    gp.append((ev, 'h', description, entry_ac))
+            elif line.startswith("EV"):
+                ev = line.split()[1].split(";")[0]
+                if ev.startswith("IPR"):
+                    gp.append((ev, "h", description, entry_ac))
 
     cur.execute(
         """
@@ -26,14 +26,15 @@ def import_gp(cur: cx_Oracle.Cursor, file_path: str):
         INSERT INTO INTERPRO.ENTRY_XREF (
         ENTRY_AC, DBCODE, AC, NAME
         ) VALUES (:1, :2, :3, :4)
-        """, gp
+        """,
+        gp,
     )
 
 
-if __name__ == '__main__':
-    con = cx_Oracle.connect('connection string')
+if __name__ == "__main__":
+    con = cx_Oracle.connect("connection string")
     cur = con.cursor()
-    import_gp(cur, 'path/to/genomeProperties.txt')
+    import_gp(cur, "path/to/genomeProperties.txt")
     cur.close()
     con.commit()
     con.close()
