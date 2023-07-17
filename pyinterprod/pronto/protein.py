b5fca04ebb7170df19e9af50b8b6ff15c9c96172
Laise Florentino <laisecflorentino@gmail.com>
2023-07-17 16:31:04 +0100

    Update pyinterprod/pronto/protein.py
    Co-authored-by: Matthias Blum <matthiasblum@users.noreply.github.com>

--- pyinterprod/pronto/protein.py
+++ pyinterprod/pronto/protein.py
@@ -293,6 +293,7 @@ def import_proteins(ora_url: str, pg_url: str):
                 pg_cur.executemany(sql, records)
                 pg_con.commit()
                 records.clear()
+
         if records:
             pg_cur.executemany(sql, records)
             pg_con.commit()
