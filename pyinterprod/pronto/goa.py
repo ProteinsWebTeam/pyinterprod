3ee224610a0f395ebac1a82c1f533f39985d2630
Laise Florentino <laisecflorentino@gmail.com>
2023-07-17 16:23:05 +0100

    Update pyinterprod/pronto/goa.py
    Co-authored-by: Matthias Blum <matthiasblum@users.noreply.github.com>

--- pyinterprod/pronto/goa.py
+++ pyinterprod/pronto/goa.py
@@ -123,6 +123,7 @@ def import_annotations(ora_url: str, pg_url: str):
                 pg_cur.executemany(sql, records)
                 pg_con.commit()
                 records.clear()
+
         if records:
             pg_cur.executemany(sql, records)
             pg_con.commit()
