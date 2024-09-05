f40f38c40ee5c952f13e9b320c9a9f25fccb6753
Matthias Blum <mat.blum@gmail.com>
2024-09-05 18:06:17 +0100

    Remove TIGRFAM

--- pyinterprod/interpro/iprscan.py
+++ pyinterprod/interpro/iprscan.py
@@ -209,16 +209,6 @@ MATCH_PARTITIONS = {
         ],
         "partition": "NCBIFAM"
     },
-    # TODO: remove once TIGRFAMs has been replaced by NCBIfam
-    "TIGRFAMs": {
-        "columns": [
-            'ANALYSIS_ID', 'UPI', 'METHOD_AC', 'RELNO_MAJOR', 'RELNO_MINOR',
-            'SEQ_START', 'SEQ_END', 'HMM_START', 'HMM_END', 'HMM_LENGTH',
-            'HMM_BOUNDS', 'SCORE', 'SEQSCORE', 'EVALUE', 'SEQEVALUE',
-            'ENV_START', 'ENV_END', 'MODEL_AC', 'NULL', 'FRAGMENTS', 'NULL'
-        ],
-        "partition": "NCBIFAM"
-    },
     "TMHMM": {
         "columns": [
             'ANALYSIS_ID', 'UPI', 'METHOD_AC', 'RELNO_MAJOR', 'RELNO_MINOR',
