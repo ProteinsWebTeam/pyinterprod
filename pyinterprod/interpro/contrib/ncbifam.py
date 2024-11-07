67428956eb601bd9522106605d7aa8e08d15c20d
Matthias Blum <mat.blum@gmail.com>
2024-11-07 17:47:39 +0000

    Import all NCBIFAM models, including AMR ones

--- pyinterprod/interpro/contrib/ncbifam.py
+++ pyinterprod/interpro/contrib/ncbifam.py
@@ -15,40 +15,24 @@ def parse_tsv(tsvfile: str):
                 yield line.split("\t")
 
 
-def get_signatures(cur: oracledb.Cursor,
-                   tsvfile: str,
-                   triagefile: str,
-                   keep_integrated: bool = True) -> list[Method]:
+def get_signatures(tsvfile: str,
+                   cur: oracledb.Cursor | None = None) -> list[Method]:
     """Parse NCBIfam TSV file
     https://ftp.ncbi.nlm.nih.gov/hmm/current/hmm_PGAP.tsv
     and returns signatures to be imported in InterPro
-    :param cur: Oracle cursor
     :param tsvfile: Path to TSV file
-    :param triagefile: Path to TXT file of families to import
-    :param keep_integrated: If True, keep integrated signatures
-           even if they are not in the list of models to import
+    :param cur: Oracle cursor or None
     :return:
     """
-    cur.execute("SELECT METHOD_AC FROM INTERPRO.ENTRY2METHOD")
-    integrated = {signature_acc for signature_acc, in cur.fetchall()}
-
-    to_import = set()
-    with open(triagefile, "rt") as fh:
-        for model_acc in map(str.rstrip, fh):
-            to_import.add(model_acc)
-
     methods = []
+    amr_models = []
     for values in parse_tsv(tsvfile):
         model_acc = values[0]
         signature_acc, model_version = model_acc.split(".")
 
-        if model_acc not in to_import:
-            if signature_acc not in integrated or not keep_integrated:
-                continue
-
         name = values[2]
         _type = values[6]
-        # for_AMRFinder = values[9] == "Y"
+        for_AMRFinder = values[9] == "Y"
 
         if values[14]:
             references = list(map(int, set(values[14].split(","))))
@@ -81,12 +65,34 @@ def get_signatures(cur: oracledb.Cursor,
         )
         methods.append(m)
 
+        if for_AMRFinder:
+            amr_models.append(signature_acc)
+
+    if cur:
+        drop_table(cur, "INTERPRO.NCBIFAM_AMR", purge=True)
+        cur.execute(
+            """
+            CREATE TABLE INTERPRO.NCBIFAM_AMR
+            (
+                METHOD_AC VARCHAR2(25) NOT NULL
+                    CONSTRAINT PK_NCBIFAM_AMR
+                        PRIMARY KEY
+                    CONSTRAINT FK_NCBIFAM_AMR
+                        REFERENCES INTERPRO.METHOD (METHOD_AC) 
+                        ON DELETE CASCADE
+            )
+            """
+        )
+        if amr_models:
+            cur.executemany("INSERT INTO INTERPRO.NCBIFAM_AMR VALUES (:1)",
+                            amr_models)
+            cur.connection.commit()
+
     return methods
 
 
-def create_custom_hmm(cur: oracledb.Cursor, tsvfile: str, triagefile: str,
-                      hmmfile: str):
-    models = {m.model: m for m in get_signatures(cur, tsvfile, triagefile)}
+def create_custom_hmm(tsvfile: str, hmmfile: str):
+    models = {m.model: m for m in get_signatures(tsvfile)}
 
     reg_acc = re.compile(r"^ACC\s+(\w+\.\d+)$", flags=re.M)
     reg_desc = re.compile(r"^(DESC\s+)(.+)$", flags=re.M)
@@ -161,28 +167,12 @@ def update_go_terms(uri: str, tsvfile: str):
 def main():
     parser = ArgumentParser(description="create a custom NCBIfam "
                                         "HMM file for InterProScan")
-    parser.add_argument("config", metavar="pyinterprod.conf",
-                        help="configuration file")
     parser.add_argument("tsv", metavar="hmm_PGAP.tsv",
                         help="signatures information, in TSV format")
-    parser.add_argument("triage", metavar="for-interpro.tsv",
-                        help="triage file of models to import")
     parser.add_argument("hmm", metavar="hmm_PGAP.LIB",
                         help="file of concatenated HMM profiles")
     args = parser.parse_args()
-
-    config = ConfigParser()
-    config.read(args.config)
-    uri = config["oracle"]["ipro-interpro"]
-
-    con = oracledb.connect(uri)
-    cur = con.cursor()
-
-    try:
-        create_custom_hmm(cur, args.tsv, args.triage, args.hmm)
-    finally:
-        cur.close()
-        con.close()
+    create_custom_hmm(args.tsv, args.hmm)
 
 
 if __name__ == "__main__":
